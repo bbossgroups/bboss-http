22db46380339257503c7ab8d395bebfe6a07a96c
yin-bp <yin-bp@163.com>
2023-05-21 20:27:41 +0800

    1.增加msearch api，支持通过msearch检索可以在一次http请求中执行多个检索请求，将每个请求的结果封装到List返回。 https://esdoc.bbossgroups.com/#/document-crud?id=_161-%e6%89%a7%e8%a1%8cmsearch

--- src/org/frameworkset/spi/remote/http/BaseResponseHandler.java
+++ src/org/frameworkset/spi/remote/http/BaseResponseHandler.java
@@ -5,6 +5,7 @@ import org.apache.http.HttpEntity;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 import static org.frameworkset.spi.remote.http.ResponseUtil.entityEmpty;
 
@@ -35,6 +36,22 @@ public abstract class BaseResponseHandler extends StatusResponseHandler {
 			inputStream.close();
 		}
 	}
+
+    protected <T> List<T> converJson2List(HttpEntity entity, Class<T> clazz) throws IOException {
+        InputStream inputStream = null;
+        try {
+
+            inputStream = entity.getContent();
+
+            if(entityEmpty(entity,inputStream)){
+                return null;
+            }
+            return SimpleStringUtil.json2ListObject(inputStream, clazz);
+        }
+        finally {
+            inputStream.close();
+        }
+    }
 
     public boolean isEnableSetRequestBody() {
         return enableSetRequestBody;
